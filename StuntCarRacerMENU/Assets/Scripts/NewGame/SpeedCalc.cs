dccb2f74f00bd502033023ef96f25c78594fde0b
Elisenaps <148022712+Elisenaps@users.noreply.github.com>
2026-04-29 18:42:40 +0800

    Speed Calculation and Text Display added
    Added SpeedCalc script that calculates the speed in KM/H and displays it in the bottom right corner in a text format.
    Also added the text to the bottom right corner of the UI, and added the SpeedCalc script to the player character.

---
+++ StuntCarRacerMENU/Assets/Scripts/NewGame/SpeedCalc.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpeedCalc : MonoBehaviour
+{
+    //Elise Avery 30128123
+    [SerializeField] Rigidbody carBody;
+    [SerializeField] TextMeshProUGUI speedDisplay;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        DisplayText();
+    }
+
+    void DisplayText()
+    {
+        //sets the speed display text to the velocity magnitude in unity measurements (meters) *3.6 to be KM/h, converted to a short string
+        speedDisplay.text = (carBody.linearVelocity.magnitude*3.6).ToShortString();
+        
+        
+    }
+}
