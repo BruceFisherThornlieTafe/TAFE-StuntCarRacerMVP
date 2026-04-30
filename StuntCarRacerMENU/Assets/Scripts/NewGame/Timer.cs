b41e9212b0c2fe04fa3c364cda560ecfa30afaa6
CGreenie <callumg345@gmail.com>
2026-04-30 10:51:51 +0800

    Timer UI Initialization
    - Created UI basic UI timer panel and text
    - Created 'Timer' script, connected script and UI element

---
+++ StuntCarRacerMENU/Assets/Scripts/NewGame/Timer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    // Callum Greenshields 30728074
+
+    [SerializeField] TextMeshProUGUI timerDisplay;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
